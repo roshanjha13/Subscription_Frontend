86346313b804ea5d43b822279c8d1a1fd37ce08b
Roshan <roshan.jha@hih7.in>
2023-04-25 18:13:25 +0530

    Login Form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
+import Login from './components/Auth/Login';
 function App() {
   return (
     <Router>
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
