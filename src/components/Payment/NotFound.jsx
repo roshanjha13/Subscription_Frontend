b65511674835025bedd688cb7b0c14b7598d3682
Roshan <roshan.jha@hih7.in>
2023-04-26 18:31:25 +0530

    Payment Error Success And Fail

--- src/components/Payment/NotFound.jsx
+++ src/components/Payment/NotFound.jsx
@@ -1,8 +1,20 @@
-import { Container } from '@chakra-ui/react';
+import { Container, Heading, VStack, Button } from '@chakra-ui/react';
 import React from 'react';
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return <Container></Container>;
+  return (
+    <Container h="90vh" p="16">
+      <VStack justifyContent={'center'} h={'full'} spacing={'4'}>
+        <RiErrorWarningFill size={'5rem'} />
+        <Heading>Page Not Found</Heading>
+        <Link to="/">
+          <Button variant={'ghost'}>Go To Home</Button>
+        </Link>
+      </VStack>
+    </Container>
+  );
 };
 
 export default NotFound;
--- src/components/Payment/PaymentSuccess.jsx
+++ src/components/Payment/PaymentSuccess.jsx
@@ -1,8 +1,48 @@
-import { Container } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Heading,
+  VStack,
+  Text,
+  Button,
+} from '@chakra-ui/react';
 import React from 'react';
+import { RiCheckboxCircleFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const PaymentSuccess = () => {
-  return <Container></Container>;
+  return (
+    <Container h="90vh" p="16">
+      <Heading my={'8'} textAlign={'center'}>
+        You have Pro Pack
+      </Heading>
+      <VStack boxShadow={'lg'} pb="16" alignItems={'center'} borderRadius="lg">
+        <Box
+          w="full"
+          bg="yellow.400"
+          p="4"
+          css={{ borderRadius: '8px 8px 0 0' }}
+        >
+          <Text color={'black'}>Payment Success</Text>
+        </Box>
+        <Box p="4">
+          <VStack textAlign={'center'} px="8" mt="4" spacing={'8'}>
+            <Text>
+              Congratulation you rae a pro member .you Have access to premium
+              content.
+            </Text>
+            <Heading size={'4xl'}>
+              <RiCheckboxCircleFill />
+            </Heading>
+            <Link to="/profile">
+              <Button variant={'ghost'}>Go To profile</Button>
+            </Link>
+            <Heading size={'xs'}>Reference :dnxkjshdjdmdm,dsfsdf,dsfds</Heading>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default PaymentSuccess;
