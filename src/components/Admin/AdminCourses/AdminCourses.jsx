c2b4c6493f7b9f2b58d876e7fbbafa7ce9340955
Roshan <roshan.jha@hih7.in>
2023-04-28 18:11:56 +0530

    Add Lecture Form,

--- src/components/Admin/AdminCourses/AdminCourses.jsx
+++ src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -1,8 +1,56 @@
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
 import React from 'react';
 import cursor from '../../../assests/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import CourseModal from './CourseModal';
 const AdminCourses = () => {
+  const courses = [
+    {
+      _id: 'asdasdfasd',
+      title: 'React Course',
+      category: 'Web Development',
+      poster: {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqAdLIdd3o_EzDbEA9bCvgbc4XpgI1txiTUQ&usqp=CAU',
+      },
+      createdBy: 'Roshan jha',
+      views: 123,
+      numOfVideos: 12,
+    },
+  ];
+
+  const courseDetailsHandler = userId => {
+    onOpen();
+  };
+
+  const deleteUserHandler = userId => {
+    console.log(userId);
+  };
+  const deleteLectureButtonHandler = (courseId, lectureId) => {
+    console.log(courseId);
+    console.log(lectureId);
+  };
+  const addLectureHandler = (e, courseId, title, description, video) => {
+    e.preventDefault();
+  };
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <Grid
       css={{
@@ -11,10 +59,85 @@ const AdminCourses = () => {
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Box></Box>
+      <Box p={['0', '8']} overflowX="auto">
+        <Heading
+          children="All Users"
+          textTransform={'uppercase'}
+          my="16"
+          textAlign={['center', 'Left']}
+        />
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size="lg">
+            <TableCaption>All Available courses in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>id</Th>
+                <Th>Poster</Th>
+                <Th>Title</Th>
+                <Th>Category</Th>
+                <Th>Creator</Th>
+                <Th isNumeric>Views</Th>
+                <Th isNumeric>Lectures</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {courses.map(item => (
+                <Row
+                  courseDetailsHandler={courseDetailsHandler}
+                  deleteUserHandler={deleteUserHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <CourseModal
+          isOpen={isOpen}
+          onClose={onClose}
+          id={'dsfsdfdsfdfg'}
+          courseTitle="React Course"
+          deleteUserHandler={deleteLectureButtonHandler}
+          addLectureHandler={addLectureHandler}
+        />
+      </Box>
       <Sidebar />
     </Grid>
   );
 };
+
+function Row({ item, courseDetailsHandler, deleteUserHandler }) {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => courseDetailsHandler(item._id)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            View Lecture
+          </Button>
+          <Button
+            onClick={() => deleteUserHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
 
 export default AdminCourses;
