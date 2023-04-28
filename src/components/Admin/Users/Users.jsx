fa864937d08fe7fee91bc0325423bd751018dbd2
Roshan <roshan.jha@hih7.in>
2023-04-28 12:16:06 +0530

    Admin User Table

--- src/components/Admin/Users/Users.jsx
+++ src/components/Admin/Users/Users.jsx
@@ -1,8 +1,43 @@
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import cursor from '../../../assests/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
 const Users = () => {
+  const users = [
+    {
+      _id: 'asdasdfasd',
+      name: 'roshan',
+      email: 'roshan@gmail.com',
+      role: 'admin',
+      subscription: {
+        status: 'active',
+      },
+    },
+  ];
+
+  const updateHandler = userId => {
+    console.log(userId);
+  };
+
+  const deleteUserHandler = userId => {
+    console.log(userId);
+  };
+
   return (
     <Grid
       css={{
@@ -11,10 +46,71 @@ const Users = () => {
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Box></Box>
+      <Box p={['0', '16']} overflowX="auto">
+        <Heading
+          children="All Users"
+          textTransform={'uppercase'}
+          my="16"
+          textAlign={['center', 'Left']}
+        />
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size="lg">
+            <TableCaption>All Available users in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>id</Th>
+                <Th>Name</Th>
+                <Th>Email</Th>
+                <Th>Role</Th>
+                <Th>Subscription</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users.map(item => (
+                <Row
+                  updateHandler={updateHandler}
+                  deleteUserHandler={deleteUserHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
       <Sidebar />
     </Grid>
   );
 };
+
+function Row({ item, updateHandler, deleteUserHandler }) {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+      <Td>{item.name}</Td>
+      <Td>{item.email}</Td>
+      <Td>{item.role}</Td>
+      <Td>{item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => updateHandler(item._id)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            Change Role
+          </Button>
+          <Button
+            onClick={() => deleteUserHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
 
 export default Users;
