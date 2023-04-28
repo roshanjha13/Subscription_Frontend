07e925399f28e26f856b2b13a9175765c7b92eb5
Roshan <roshan.jha@hih7.in>
2023-04-28 10:56:29 +0530

    DashBoard Charts

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -11,6 +11,7 @@ import React from 'react';
 import cursor from '../../../assests/images/cursor.png';
 import Sidebar from '../Sidebar';
 import { RiArrowDownLine, RiArrowUpLine } from 'react-icons/ri';
+import { DoughnutChart, LineChart } from './Chart';
 
 const Databox = ({ title, qty, qtyPercentage, profit }) => (
   <Box
@@ -94,7 +95,9 @@ const Dashboard = () => {
             pt={['8', '0']}
             ml={['0', '16']}
           />
+          <LineChart />
         </Box>
+
         <Grid templateColumns={['1fr', '2fr 1fr']}>
           <Box p="4">
             <Heading
@@ -112,6 +115,8 @@ const Dashboard = () => {
           </Box>
           <Box p={['0', '16']} boxSizing="border-box" py="4">
             <Heading textAlign={'center'} size={'md'} mb="4" children="Users" />
+
+            <DoughnutChart />
           </Box>
         </Grid>
       </Box>
